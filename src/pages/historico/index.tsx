e15208d693730e681e92968ab2e36916b0f345ed
Lemillions <marcosvsdxd@gmail.com>
2022-05-06 00:13:42 -0300

    BORAAAAAA, mas ta feio

--- src/pages/historico/index.tsx
+++ src/pages/historico/index.tsx
@@ -1,19 +1,41 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UserContext } from '../../contexts/index'
 import styles from "./styles.module.scss"
 
+type merda = Array<number>
 
 export default function Historico(){
   const { state }= useContext(UserContext)
-  console.log(state)
+  const [listaHistoricoMostrar, setiLstaHistoricoMostrar] = useState<merda>([])
+  var copia:merda = [...listaHistoricoMostrar]
+  const mostrarProdutos = (index:number) => {
+    if(listaHistoricoMostrar.includes(index)){
+      copia.splice(copia.indexOf(index), 1)
+      setiLstaHistoricoMostrar(copia)
+    }else{
+      setiLstaHistoricoMostrar([...listaHistoricoMostrar, index])
+    }
+  }
+
   return (  
     <div className={styles.historicoContainer}>
       {state.historico.map((venda, index)=>{
         return ( 
-          <li className={styles.venda} key={index}>
+          <>
+          <li className={styles.venda} key={index} onClick={()=>{mostrarProdutos(index)}}>
             <div className={styles.quantProdutos}>{venda.produtos.length} Produtos</div>
             <div className={styles.dataDaVenda}>{venda.data}</div>
           </li>
+          {!listaHistoricoMostrar.includes(index)?"":state.historico[index].produtos.map(produto =>{
+            return (
+              <div key={index+produto.nome} className={styles.produtosListadoHistorico}>
+                <div className={styles.nomeListado}>{produto.nome}</div>
+                <div className={styles.quantidadeListado}>{produto.quantidade}</div>
+                <div className={styles.valorListado}>{produto.valor}</div>
+              </div>
+               )
+          })}
+          </>
         )
       })}
     </div>
